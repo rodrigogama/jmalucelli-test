80572be557eb04496aed7c48ba69cfcabda962cd
Rodrigo Gama Baptista <rodrigo.baptista@prestadores.amil.com.br>
2018-07-11 13:22:25 -0300

    added avatar tests

---
+++ src/components/Avatar/Avatar.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+import Avatar from './Avatar';
+import image from '../../assets/img/img_avatar.png';
+import image2 from '../../assets/img/img_avatar2.png';
+
+describe('Avatar Component', () => {
+  it('should render without throwing an error', () => {
+    expect(shallow(<Avatar src={image}/>).find('.avatar-container').exists()).toBe(true);
+  });
+
+  it('should render the image passed as props', () => {
+    const wrapper = mount(<Avatar src={image2}/>);
+    const imgSrc = wrapper.find('.avatar-container').find('.avatar-img').prop('src');
+    expect(imgSrc).toEqual(image);
+  });
+});
